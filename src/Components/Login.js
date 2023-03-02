e5cb4d5c680966f837ce6b6f84eb678caa7c2429
Siddharth Jain <siddharthjainjio@gmail.com>
2023-03-03 00:38:33 +0530

    Add useReducer hook

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,36 +1,92 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { auth } from '../Config/Config'
 import { Link, useNavigate } from 'react-router-dom'
 
 function Login() {
     const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
 
-    const [errorMsg, setErrorMsg] = useState('');
-    const [successMsg, setSuccessMsg] = useState('');
+    // const [errorMsg, setErrorMsg] = useState('');
+    // const [successMsg, setSuccessMsg] = useState('');
+
+    function reducer(state, action) {
+        if (action.type === 'setEmail') {
+            return {
+                email: state.email
+            };
+        }
+        if (action.type === 'setPassword') {
+            return {
+                password: state.password
+            };
+        }
+        if (action.type === 'setErrorMsg') {
+            return {
+                errorMsg: state.errorMsg
+            };
+        }
+        if (action.type === 'setSuccessMsg') {
+            return {
+                successMsg: state.successMsg
+            };
+        }
+        throw Error('Unknown action.');
+    }
+
+    const [state, dispatch] = useReducer(reducer, {email: '', password: '', errorMsg: '', successMsg: ''})
 
     const handleLogin = (e) => {
         e.preventDefault();
         // console.log(email,password)
-        auth.signInWithEmailAndPassword(email, password).then(() => {
-            setSuccessMsg('Login succesful. You will now automatically get redirected to Home')
-            setEmail('');
-            setPassword('');
-            setErrorMsg('');
+        auth.signInWithEmailAndPassword(state.email, state.password).then(() => {
+            // setSuccessMsg('Login succesful. You will now automatically get redirected to Home')
+            // setEmail('');
+            // setPassword('');
+            // setErrorMsg('');
+            dispatch({
+                type: 'setSuccessMsg',
+                nextName: 'Login succesful. You will now automatically get redirected to Home'
+            })
+            dispatch({
+                type: 'setErrorMsg',
+                nextName: ''
+            })
+            dispatch({
+                type: 'setEmail',
+                nextName: ''
+            })
+            dispatch({
+                type: 'setPassword',
+                nextName: ''
+            })
             setTimeout(() => {
-                setSuccessMsg('');
+                // setSuccessMsg('');
+                dispatch({
+                    type: 'setSuccessMsg',
+                    nextName: ''
+                })
                 navigate('/');
             },) // to add delay 3000
         }).catch((error) => {
-            setErrorMsg(error.message)
+            // setErrorMsg(error.message)
+            dispatch({
+                type: 'setErrorMsg',
+                nextName: error.message
+            })
         })
 
     }
     return (<>
         <div className="container mt-5">
-            {successMsg && <>
-                <div className='alert alert-success alert-dismissible fade show'>{successMsg}
+            {state.successMsg && <>
+                <div className='alert alert-success alert-dismissible fade show'>{state.successMsg}
+                </div>
+                <br></br>
+            </>}
+            {state.errorMsg && <>
+                <div className='alert alert-danger alert-dismissible fade show'>
+                    {state.errorMsg}
                 </div>
                 <br></br>
             </>}
@@ -39,12 +95,24 @@ function Login() {
                 <div className="mb-3">
                     <label htmlFor="email" className="form-label">Email address</label>
                     <input type="email" className="form-control" id="email" placeholder="name@example.com"
-                        onChange={(e) => setEmail(e.target.value)} value={email} />
+                        onChange={(e) =>
+                        // setEmail(e.target.value)
+                        dispatch({
+                            type: 'setEmail',
+                            nextName: e.target.value
+                        })
+                    } value={state.email} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="password" className="form-label">Password</label>
                     <input type="password" className="form-control" id="password" placeholder="12345678"
-                        onChange={(e) => setPassword(e.target.value)} value={password} />
+                        onChange={(e) => 
+                        //setPassword(e.target.value)
+                        dispatch({
+                            type: 'setPassword',
+                            nextName: e.target.value
+                        })
+                    } value={state.password} />
                 </div>
                 <div className="mb-3">
                     <span>Don't have an account Signup
@@ -54,12 +122,6 @@ function Login() {
                     <button type="submit" className="btn btn-primary">Login</button>
                 </div>
             </form>
-            {errorMsg && <>
-                <div className='alert alert-danger alert-dismissible fade show'>
-                    {errorMsg}
-                </div>
-                <br></br>
-            </>}
         </div>
     </>
 
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react' //useState also 
 import { auth, fs } from '../Config/Config'
 import { Link, useNavigate } from 'react-router-dom'
 
@@ -6,80 +6,160 @@ function Signup() {
 
     const navigate = useNavigate();
 
-    const [uname, setUname] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    function reducer(state, action) {
+        if (action.type === 'setUname') {
+            return {
+                uname: state.uname
+            };
+        }
+        if (action.type === 'setEmail') {
+            return {
+                email: state.email
+            };
+        }
+        if (action.type === 'setPassword') {
+            return {
+                password: state.password
+            };
+        }
+        if (action.type === 'setErrorMsg') {
+            return {
+                errorMsg: state.errorMsg
+            };
+        }
+        if (action.type === 'setSuccessMsg') {
+            return {
+                successMsg: state.successMsg
+            };
+        }
+        throw Error('Unknown action.');
+    }
 
-    const [errorMsg, setErrorMsg] = useState('');
-    const [successMsg, setSuccessMsg] = useState('');
+    // const [uname, setUname] = useState('');
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+
+    // const [errorMsg, setErrorMsg] = useState('');
+    // const [successMsg, setSuccessMsg] = useState('');
+
+    const [state, dispatch] = useReducer(reducer, { uname: '', email: '', password: '', errorMsg: '', successMsg: '' })
 
     const handleSignup = (e) => {
         e.preventDefault();
         //   console.log(uname,email,password);
-        auth.createUserWithEmailAndPassword(email, password).then((credentials) => {
+        auth.createUserWithEmailAndPassword(state.email, state.password).then((credentials) => {
             console.log(credentials);
             fs.collection('users').doc(credentials.user.uid).set({
-                UserName: uname,
-                Email: email,
-                Password: password
+                UserName: state.uname,
+                Email: state.email,
+                Password: state.password
             }).then(() => {
-                setSuccessMsg('Signup succesful. You will now automatically get redirected to Login');
-                setUname('');
-                setEmail('');
-                setPassword('');
-                setErrorMsg('');
+                // setSuccessMsg('Signup succesful. You will now automatically get redirected to Login');
+                // setUname('');
+                // setEmail('');
+                // setPassword('');
+                // setErrorMsg('');
+
+                dispatch({
+                    type: 'setSuccessMsg',
+                    nextName: 'Signup succesful. You will now automatically get redirected to Login'
+                })
+                dispatch({
+                    type: 'setErrorMsg',
+                    nextName: ''
+                })
+                dispatch({
+                    type: 'setUname',
+                    nextName: ''
+                })
+                dispatch({
+                    type: 'setEmail',
+                    nextName: ''
+                })
+                dispatch({
+                    type: 'setPassword',
+                    nextName: ''
+                })
                 setTimeout(() => {
-                    setSuccessMsg('');
+                    // setSuccessMsg('');
+                    dispatch({
+                        type: 'setSuccessMsg',
+                        nextName: ''
+                    })
                     navigate('/login');
                 }, 3000)
             }).catch((error) => {
-                setErrorMsg(error.message)
+                // setErrorMsg(error.message)
+                dispatch({
+                    type: 'setErrorMsg',
+                    nextName: error.message
+                })
             })
         }).catch((error) => {
-            setErrorMsg(error.message)
+            // setErrorMsg(error.message)
+            dispatch({
+                type: 'setErrorMsg',
+                nextName: error.message
+            })
         })
     }
     return (
         <>
-        <div className="container">
-            <div className="mt-5">
-                {successMsg && <>
-                    <div className='alert alert-success alert-dismissible fade show'>{successMsg}
-                    </div>
-                    <br></br>
-                </>}
-                <h1>Signup</h1>
-                <form onSubmit={handleSignup}>
-                    <div className="mb-3">
-                        <label htmlFor="uname" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="uname" placeholder="name"
-                            onChange={(e) => setUname(e.target.value)} value={uname} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email address</label>
-                        <input type="email" className="form-control" id="email" placeholder="name@example.com"
-                            onChange={(e) => setEmail(e.target.value)} value={email} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="12345678"
-                            onChange={(e) => setPassword(e.target.value)} value={password} />
-                    </div>
-                    <div className="mb-3">
-                        <span>Already have an account Login
-                            <Link to='/login'>Here</Link></span>
-                    </div>
-                    <div className="mb-3">
-                        <button type="submit" className="btn btn-primary">SignUp</button>
-                    </div>
-                </form>
-                {errorMsg && <>
-                    <div className='alert alert-danger alert-dismissible fade show'>
-                        {errorMsg}
-                    </div>
-                    <br></br>
-                </>}
-            </div>
+            {state.errorMsg && <>
+                <div className='alert alert-danger alert-dismissible fade show'>
+                    {state.errorMsg}
+                </div>
+                <br></br>
+            </>}
+            <div className="container">
+                <div className="mt-5">
+                    {state.successMsg && <>
+                        <div className='alert alert-success alert-dismissible fade show'>{state.successMsg}
+                        </div>
+                        <br></br>
+                    </>}
+                    <h1>Signup</h1>
+                    <form onSubmit={handleSignup}>
+                        <div className="mb-3">
+                            <label htmlFor="uname" className="form-label">Name</label>
+                            <input type="text" className="form-control" id="uname" placeholder="name"
+                                onChange={
+                                    (e) => // setUname(e.target.value)
+                                        dispatch({
+                                            type: 'setUname',
+                                            nextName: e.target.value
+                                        })
+                                } value={state.uname} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email address</label>
+                            <input type="email" className="form-control" id="email" placeholder="name@example.com"
+                                onChange={(e) => // setEmail(e.target.value)
+                                    dispatch({
+                                        type: 'setEmail',
+                                        nextName: e.target.value
+                                    })
+                                } value={state.email} />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="password" placeholder="12345678"
+                                onChange={(e) => // setPassword(e.target.value)
+                                    dispatch({
+                                        type: 'setPassword',
+                                        nextName: e.target.value
+                                    })
+                                } value={state.password} />
+                        </div>
+                        <div className="mb-3">
+                            <span>Already have an account Login
+                                <Link to='/login'>Here</Link></span>
+                        </div>
+                        <div className="mb-3">
+                            <button type="submit" className="btn btn-primary">SignUp</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </>
     )
