c41839773799b9eecc15c26c23eed0168dc1a1bb
Siddharth Jain <siddharthjainjio@gmail.com>
2023-03-07 23:47:17 +0530

    Add Signup auth

---
+++ src/Libs/firebaseutils.js
@@ -0,0 +1,60 @@
+import { auth, fs } from '../Config/Config';
+
+export const signUpWithEmailAndPassword = async (uname, email, password, dispatch, navigate) => {
+    auth.createUserWithEmailAndPassword(email, password).then((credentials) => {
+        fs.collection('users').doc(credentials.user.uid).set({
+            UserName: uname,
+            Email: email,
+            Password: password
+        }).then(() => {
+            dispatch({
+                type: 'setSuccessMsg',
+                value: 'Signup succesful. You will now automatically get redirected to Home'
+            })
+            dispatch({
+                type: 'setErrorMsg',
+                value: ''
+            })
+            dispatch({
+                type: 'setUname',
+                value: ''
+            })
+            dispatch({
+                type: 'setEmail',
+                value: ''
+            })
+            dispatch({
+                type: 'setPassword',
+                value: ''
+            })
+            setTimeout(() => {
+                // setSuccessMsg('');
+                dispatch({
+                    type: 'setSuccessMsg',
+                    value: ''
+                })
+                // console.log(state.email, state.password, state.successMsg)
+                navigate('/');
+            }, 600)
+        }).catch((error) => {
+            // setErrorMsg(error.message)
+            let errorString = error.message;
+            errorString = errorString.replace("Firebase: ", "");
+            errorString = errorString.replace(/ \(auth\/.*?\)\./, "");
+
+            dispatch({
+                type: 'setErrorMsg',
+                value: errorString
+            })
+        })
+    }).catch((error) => {
+        // setErrorMsg(error.message)
+        let errorString = error.message;
+        errorString = errorString.replace("Firebase: ", "");
+        errorString = errorString.replace(/ \(auth\/.*?\)\./, "");
+        dispatch({
+            type: 'setErrorMsg',
+            value: errorString
+        })
+    })
+};
